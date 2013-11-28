8f705bbc21eb8072a4751dae77a84d1367a15047
kravchuk-ievgenii <kravchuk.ievgenii@gmail.com>
2013-11-28 23:08:16 +0200

    helpers for views added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def time_now
+   Time.new
+  end
+
+  def quantity_users
+    quantity = @users.length
+  end 
+
+  def quantity_posts
+    quantity = @posts.length 
+  end
+  
 end
