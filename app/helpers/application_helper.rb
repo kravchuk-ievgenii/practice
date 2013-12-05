15b0216724dab39b9a14692262c20ee41923f1c9
kravchuk-ievgenii <kravchuk.ievgenii@gmail.com>
2013-12-05 17:00:41 +0200

    some changes in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
 
   def time_now
-   Time.now.strftime("%d/%m/%Y %H:%M")
+   Time.now.strftime("%H:%M")
   end
 
   def quantity_users
     quantity = @users.length
   end 
 
-  def quantity_posts
-    quantity = @posts.length 
+  def quantity_posts_posts
+    quantity = @user.posts.count
   end
-  
+   def quantity_posts (user)
+    quantity = user.posts.count
+  end 
 end
